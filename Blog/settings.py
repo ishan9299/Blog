496e29932ac517a0e2a543e557a39ef434dd1e70
ishan <ishan365.ia@gmail.com>
2024-07-29 18:08:29 +0530

    try vercel

--- Blog/settings.py
+++ Blog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".railway.app", "ishanagarwal.xyz"]
+ALLOWED_HOSTS = ["127.0.0.1", ".railway.app", "ishanagarwal.xyz", ".vercel.app"]
 
 
 # Application definition
