ece85482ee754e438c85b8435996e5994c91effc
ishan <ishan365.ia@gmail.com>
2024-07-29 12:43:53 +0530

    change vercel to railway

--- Blog/settings.py
+++ Blog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["127.0.0.1", ".railway.app", "ishanagarwal.xyz"]
 
 
 # Application definition
