69892ff0b2659b55f68e4a9800c8e75a7dd3c93e
ishan <ishan365.ia@gmail.com>
2025-01-17 14:34:02 +0530

    remove blogs for now.

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,7 +5,7 @@ from blog_app.views import home_page, list_articles, markdown_content_view, proj
 
 urlpatterns = [
         path("", home_page, name="home_page"),
-        path("blog/", list_articles, name="list_articles"),
-        path("blog/<slug:slug>/", markdown_content_view, name="markdown_content"),
+        # path("blog/", list_articles, name="list_articles"),
+        # path("blog/<slug:slug>/", markdown_content_view, name="markdown_content"),
         path("projects/", projects_view, name="projects_view"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -8,16 +8,16 @@ def home_page(request):
     except:
         return HttpResponse(f"Template not found: {e}")
 
-def list_articles(request):
-    articles = MarkdownContent.objects.values_list('title', 'slug', 'created')
-    articles = articles.order_by('-created')
-    context = {"articles": articles}
-    return render(request, "blog_app/list_articles.html", context=context)
-
-def markdown_content_view(request, slug):
-    markdown_content = get_object_or_404(MarkdownContent, slug=slug)
-    context = {"markdown_content": markdown_content}
-    return render(request, "blog_app/markdown_content.html", context=context)
+# def list_articles(request):
+#     articles = MarkdownContent.objects.values_list('title', 'slug', 'created')
+#     articles = articles.order_by('-created')
+#     context = {"articles": articles}
+#     return render(request, "blog_app/list_articles.html", context=context)
+#
+# def markdown_content_view(request, slug):
+#     markdown_content = get_object_or_404(MarkdownContent, slug=slug)
+#     context = {"markdown_content": markdown_content}
+#     return render(request, "blog_app/markdown_content.html", context=context)
 
 def projects_view(request):
     return render(request, "blog_app/projects.html")
